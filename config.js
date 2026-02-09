b44cf31af53f017b8be535b67f01b51ebbc29d32
venom <99524294+Vinodmarne@users.noreply.github.com>
2026-02-09 23:30:10 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// This file is PRIVATE. Do not upload this to GitHub.
+const CONFIG = {
+    API_KEY: "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",
+    MODEL: "google/gemini-2.0-flash-001"
+};
